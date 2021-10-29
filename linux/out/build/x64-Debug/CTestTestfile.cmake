24140759172fd9d7fcd6d83daf1dd1a7084bb374
Еганов Денис Вадимович <eden@agentura-soft.ru>
2021-10-29 20:07:55 +0300

    test

---
+++ linux/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/projects/PinkRabbitMQ/linux
+# Build directory: E:/projects/PinkRabbitMQ/linux/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "unittest")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "E:/projects/PinkRabbitMQ/linux/CMakeLists.txt;46;add_test;E:/projects/PinkRabbitMQ/linux/CMakeLists.txt;0;")
